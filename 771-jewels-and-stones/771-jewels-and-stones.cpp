0b4eeb6c3ef49a8efed167529e6b151ebb4802a0
Abinash462 <84255812+Abinash462@users.noreply.github.com>
2022-10-13 08:49:34 +0530

    Time: 4 ms (48.40%), Space: 6.4 MB (43.13%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -1,18 +1,23 @@
-class Solution {
-public:
-    int numJewelsInStones(string jewels, string stones) {
-        unordered_map<int,int> ans;
-        int count=0;
-        
-        for(char c:stones){
-            ans[c]++;
-        }
-        
-        for(char c:jewels){
-            if(ans.count(c)){
-                count+=ans[c];
+class Solution
+{
+    public:
+        int numJewelsInStones(string jewels, string stones)
+        {
+            unordered_map<int, int> ans;
+            int count = 0;
+
+            for (char c: stones)
+            {
+                ans[c]++;
             }
+
+            for (char c: jewels)
+            {
+                if (ans.count(c))
+                {
+                    count += ans[c];
+                }
+            }
+            return count;
         }
-        return count;
-    }
 };
