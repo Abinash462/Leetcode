fe7dc693285741c5bb39ca2bb94acd3f0311f550
Abiansh Rout <84255812+Abinash462@users.noreply.github.com>
2022-11-21 11:46:30 +0530

    Time: 25 ms (95.77%), Space: 22.2 MB (91.82%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,16 +1,24 @@
-class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int> ans;
-        
-        for(auto x:nums){
-            ans[x]++;
-        }
-        for(auto y:ans){
-            if(y.second==1){
-                return y.first;
+class Solution
+{
+    public:
+        int singleNonDuplicate(vector<int> &nums)
+        {
+            int start = 0;
+            int end = nums.size() - 2;
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] == nums[mid ^ 1])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+                mid = start + (end - start) / 2;
             }
+            return nums[start];
         }
-        return -1;
-    }
 };
