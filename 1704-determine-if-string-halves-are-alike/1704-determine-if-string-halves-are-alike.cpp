62c2b1adbd2c8032e3938d21a7396e14efe1701a
Abiansh Rout <84255812+Abinash462@users.noreply.github.com>
2022-12-01 19:08:54 +0530

    Time: 4 ms (73.06%), Space: 6.6 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,18 +1,24 @@
-class Solution {
-public:
-    bool halvesAreAlike(string s) {
-        string vowels = "aeiouAEIOU";
-        int mid=s.size()/2;
-        int ans=0;
-        
-        for(int i=0,j=mid;i<mid;i++,j++){
-            if (~vowels.find(s[i])){
-                ans++;
+class Solution
+{
+    public:
+        bool halvesAreAlike(string s)
+        {
+
+            string vowels = "aeiouAEIOU";
+            int mid = s.size() / 2;
+            int ans = 0;
+
+            for (int i = 0, j = mid; i < mid; i++, j++)
+            {
+                if (~vowels.find(s[i]))
+                {
+                    ans++;
+                }
+                if ((~vowels.find(s[j])))
+                {
+                    ans--;
+                }
             }
-            if (~vowels.find(s[j])){
-                 ans--;
-            }
+            return ans == 0;
         }
-        return ans==0;
-    }
 };
